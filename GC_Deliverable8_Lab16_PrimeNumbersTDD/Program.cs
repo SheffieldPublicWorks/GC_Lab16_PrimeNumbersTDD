a18bbad452972f5e89fe65612138c76515741b6d
unknown <stephen.scobie@gmail.com>
2017-10-31 22:11:16 -0400

    Fleshed out the remaining Program.cs file after TTD on cyber-dojo

--- GC_Deliverable8_Lab16_PrimeNumbersTDD/Program.cs
+++ GC_Deliverable8_Lab16_PrimeNumbersTDD/Program.cs
@@ -20,6 +20,53 @@ namespace GC_Deliverable8_Lab16_PrimeNumbersTDD
             Console.WriteLine("/* Welcome to the Primo Primaries v1.0 */");
             Console.WriteLine("/***************************************/");
 
+            Console.WriteLine();
+            Console.WriteLine("Let's locate some primes!");
+            Console.WriteLine();
+            Console.WriteLine("This program will find you any prime number, in order, from first prime number on.");
+            Console.WriteLine($"There are currently {primeNumbers.Length} numbers in the PrimesDB.");
+
+            bool continueStatus = true;
+            while (continueStatus)
+            {
+                Console.Write("Which prime number are you looking for? : ");
+                int n;
+                bool valid = int.TryParse(Console.ReadLine(), out n);
+
+                if (!valid || n < 1 || n > primeNumbers.Length)
+                {
+                    Console.WriteLine("Oops! It looks like that entry is invalid.");
+                    Console.Write("Do you want to find another prime number? (y/n): ");
+                    if (Console.ReadLine().ToLower() == "y")
+                    {
+                        continueStatus = true;
+                    }
+                    else
+                    {
+                        continueStatus = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(PrimeNumLookUp(primeNumbers, n));
+                    Console.Write("Do you want to find another prime number? (y/n): ");
+                    if (Console.ReadLine().ToLower() == "y")
+                    {
+                        continueStatus = true;
+                    }
+                    else
+                    {
+                        continueStatus = false;
+                    }
+                }
+            }
+
+            Console.WriteLine("Good Bye!");
+        }
+
+        public static string PrimeNumLookUp (int[] primeNumbers, int n)
+        {
+            return "The number " + n + " prime is " + primeNumbers[n - 1];
         }
     }
 }
